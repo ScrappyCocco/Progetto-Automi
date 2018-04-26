afb25428e73157f55986ccda2566080978bcabf9
ScrappyCocco <mikelerogeratro@yahoo.it>
2018-04-26 23:28:28 +0200

    Modificato il progetto con i percorsi relativi
    DOVREBBE ANDARE OSTREGA

--- Esercizi/ProveVarie/VisualStudioAntlrProject/VisualStudioAntlrProject/syncheck.cpp
+++ Esercizi/ProveVarie/VisualStudioAntlrProject/VisualStudioAntlrProject/syncheck.cpp
@@ -4,14 +4,35 @@
 #include "antlr4-runtime.h"
 #include "swlLexer.h"
 #include "swlParser.h"
+#include <windows.h>
 
-using namespace std;
 using namespace antlr4;
 
+inline bool exists_file(const std::string& name) {
+	struct stat buffer;
+	return (stat(name.c_str(), &buffer) == 0);
+}
+
+std::string workingdir()
+{
+	char buf[256];
+	GetCurrentDirectoryA(256, buf);
+	return std::string(buf) + '\\';
+}
+
 int main() {
-	cout << "Started" << endl;
-	string filename = "example.swl";
-	ifstream swlFile(filename);
+	std::cout << "Started" << std::endl;
+	std::string filename = "example.swl";
+	if (!exists_file(filename)) {
+		std::cout << "File "<< filename <<" not found in directory:"<< std::endl << (workingdir()) << std::endl;
+		fflush(stdin);
+		getchar();
+		return 1;
+	}
+	else {
+		std::cout << "File " << filename << " exist and is begin checked" << std::endl;
+	}
+	std::ifstream swlFile(filename);
 	ANTLRInputStream input(swlFile);
 	swlLexer lexer(&input);
 	CommonTokenStream tokens(&lexer);
@@ -19,9 +40,12 @@ int main() {
 	tree::ParseTree *tree = parser.program();
 	int errors = parser.getNumberOfSyntaxErrors();
 	if (errors > 0) {
-		cout << errors << " syntax errors found." << endl;
+		std::cout << errors << " syntax errors found." << std::endl;
+		fflush(stdin);
+		getchar();
 		return 1;
 	}
-	cout << "No syntax errors found." << endl;
-	return 0;
+	std::cout << "No syntax errors found." << std::endl;
+	fflush(stdin);
+	getchar();
 }
